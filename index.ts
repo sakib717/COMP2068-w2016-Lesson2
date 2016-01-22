2a2db6c4cdf94fa09c2ca514d806f5eac7f977a1
sakib717 <sakibsabbir7@gmail.com>
2016-01-22 00:47:26 -0500

    Initial Commit

---
+++ index.ts
@@ -0,0 +1,13 @@
+import http = require('http');
+
+var hostname:string = '127.0.0.1';
+var port:number = 8081;
+
+http.createServer(function(req:http.ServerRequest, res:http.ServerResponse){
+    res.writeHead(200, { 'Content-Type': 'text/plain'});
+    res.end("Hello World\n");
+}).listen(port, hostname, function(){
+      console.log("Server running at http://" + hostname + ":" + port);
+});
+
+
